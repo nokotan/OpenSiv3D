2539c111b25cafe8a291be3bcd886b34bbc25adf
kamenokonokotan <kamenokonokotan@gmail.com>
2021-06-20 01:48:31 +0900

    [Web] Update template main.cpp

--- Web/VisualStudioTemplate/OpenSiv3D(0.4.3)Web/Main.cpp
+++ Web/VisualStudioTemplate/OpenSiv3D(0.4.3)Web/Main.cpp
@@ -1,52 +1,47 @@
+
 # include <Siv3D.hpp> // OpenSiv3D v0.4.3
-# include <emscripten.h>
-
-// 猫の座標
-Vec2 catPos(640, 450);
-
-void MainLoop()
-{
-    if (!System::Update()) {
-        emscripten_cancel_main_loop();
-        return;
-    }
-
-    // テキストを画面の中心に描く
-    FontAsset(U"Default")(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
-
-    // 大きさをアニメーションさせて猫を表示する
-    TextureAsset(U"cat").resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
-
-    // マウスカーソルに追従する半透明の赤い円を描く
-    Circle(Cursor::Pos(), 50).draw(ColorF(1, 0, 0, 0.5));
-
-    // [A] キーが押されたら
-    if (KeyA.down())
-    {
-        // Hello とデバッグ表示する
-        Print << U"Hello!";
-    }
-
-    // ボタンが押されたら
-    if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
-    {
-        // 猫の座標を画面内のランダムな位置に移動する
-        catPos = RandomVec2(Scene::Rect());
-    }
-}
 
 void Main()
 {
-    // 背景を水色にする
-    Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+	// 背景を水色にする
+	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
 
-    // 大きさ 60 のフォントを用意
-    FontAsset::Register(U"Default", 60);
+	// 大きさ 60 のフォントを用意
+	const Font font(60);
 
-    // 猫のテクスチャを用意
-    TextureAsset::Register(U"cat", Emoji(U"🐈"));
+	// 猫のテクスチャを用意
+	const Texture cat(Emoji(U"🐈"));
 
-    emscripten_set_main_loop(&MainLoop, 0, 1);
+	// 猫の座標
+	Vec2 catPos(640, 450);
+
+	Platform::Web::System::SetMainLoop([&]
+	{
+        System::Update();
+
+		// テキストを画面の中心に描く
+		font(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
+
+		// 大きさをアニメーションさせて猫を表示する
+		cat.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
+
+		// マウスカーソルに追従する半透明の赤い円を描く
+		Circle(Cursor::Pos(), 40).draw(ColorF(1, 0, 0, 0.5));
+
+		// [A] キーが押されたら
+		if (KeyA.down())
+		{
+			// Hello とデバッグ表示する
+			Print << U"Hello!";
+		}
+
+		// ボタンが押されたら
+		if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
+		{
+			// 猫の座標を画面内のランダムな位置に移動する
+			catPos = RandomVec2(Scene::Rect());
+		}
+	});
 }
 
 //
@@ -55,9 +50,8 @@ void Main()
 // 実行速度が遅いと感じた場合は Release ビルドを試しましょう。
 // アプリをリリースするときにも、Release ビルドにするのを忘れないように！
 //
-// OpenSiv3D Linux 版で困ったときは、
-// Siv3D ユーザコミュニティ Slack の #linux チャンネルへ！
-// Linux 版の開発協力者も募集中です。
+// 思ったように動作しない場合は「デバッグの開始」でプログラムを実行すると、
+// 出力ウィンドウに詳細なログが表示されるので、エラーの原因を見つけやすくなります。
 //
 // = お役立ちリンク =
 //
